418f9c0e28c04d4c7762b34a32e543e8e86e5f89
Caleb Jares <cable729@gmail.com>
2013-07-08 01:58:18 -0600

    Added health and crappy text display

--- Mezzola/MezzolaGame.cs
+++ Mezzola/MezzolaGame.cs
@@ -7,6 +7,7 @@ using Mezzola.Engine.Systems;
 using Mezzola.Engine.View;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Mezzola
 {
@@ -15,6 +16,8 @@ namespace Mezzola
         private Camera3D camera;
         private readonly EntityWorld world;
         private readonly InputManager input;
+        private SpriteFont simpleFont;
+        private SpriteBatch batch;
 
         public MezzolaGame()
         {
@@ -36,28 +39,38 @@ namespace Mezzola
         protected override void LoadContent()
         {
             camera = new Camera3D(GraphicsDevice, new Vector3(0, 0, 25)) {FarClip = 50f};
+            simpleFont = Content.Load<SpriteFont>("SimpleFont");
+            batch = new SpriteBatch(GraphicsDevice);
 
             EntitySystem.BlackBoard.SetEntry("Camera", camera);
+            EntitySystem.BlackBoard.SetEntry("SimpleFont", simpleFont);
+            EntitySystem.BlackBoard.SetEntry("SpriteBatch", batch);
             EntitySystem.BlackBoard.SetEntry("GraphicsDevice", GraphicsDevice);
             EntitySystem.BlackBoard.SetEntry("Input", input);
+            
+            SetupComponents();
 
+            base.LoadContent();
+        }
+
+        private void SetupComponents()
+        {
             world.InitializeAll(true);
 
             var player = world.CreateEntity();
             player.AddComponent(new TriangleComponent(Color.Red));
-            player.AddComponent(new PositionComponent {Position = new Vector3(0, 0, 0)});
+            player.AddComponent(new PositionComponent { Position = new Vector3(0, 0, 0) });
             player.AddComponent(new MovementComponent { MovementSpeed = 6f });
             player.AddComponent(new PlayerInputComponent());
+            player.AddComponent(new HealthComponent{CurrentHealth=50,MaxHealth=50});
 
             var computer = world.CreateEntity();
             computer.AddComponent(new TriangleComponent(Color.Blue));
             computer.AddComponent(new PositionComponent { Position = new Vector3(4, -4, 0) });
-            
+
             var computer2 = world.CreateEntity();
             computer2.AddComponent(new TriangleComponent(Color.Green));
             computer2.AddComponent(new PositionComponent { Position = new Vector3(4, 4, 0) });
-
-            base.LoadContent();
         }
 
         protected override void Update(GameTime gameTime)
